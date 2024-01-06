55a7b87368918c5de0e433d610e09de1b692e975
Yamisagi <erencandan20@gmail.com>
2024-01-06 14:41:03 +0300

    feat: Handle complex form submission & Update form layout

--- src/components/ComplexForm.tsx
+++ src/components/ComplexForm.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
 
 const ComplexForm = () => {
+  // Bu kısımda görece olarak daha karmaşık bir form oluşturduk.
+  // Ve önceki örnekteki gibi bir submit fonksiyonu oluşturduk.
+  // Fakat bu sefer 1-2 adet inputla uğraşmak yerine daha fazla,
+  // ve daha farklı inputları handle etmek zorunda kalacağız.
+
+  // Bu yüzden Ref veya State kullanmak bu ve bunun gibi durumlarda
+  // Çok mantıklı olmayacaktır. Çünkü her bir input için ayrı ayrı
+  // State veya Ref oluşturmak yerine, bir form submit edildiğinde
+  // Form içindeki tüm inputların değerlerini almak daha mantıklı olacaktır.
+
+  // Bundan dolayı bize Browser tarafından sağlanan FormData API'sini kullanacağız.
+
+  const handleComplexFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Form submit edildiğinde, form içindeki tüm inputların değerlerini almak için
+
+    const formData = new FormData(e.currentTarget); // İçine form elementini veriyoruz.
+
+    // Fakat burada bir problem var. Multiple inputların aynı name'e sahip olması.
+    // Örneğin, password ve confirm password inputları aynı name'e sahip. Veya acquisition checkboxları.
+    // Bu yüzden, form içindeki tüm inputların değerlerini almak için
+
+    const acquisition = formData.getAll('acquisition'); // FormData.getAll() ile aynı name'e sahip tüm inputların değerlerini alıyoruz.
+
+    // Ilk önce datayı toplayıp daha sonra bu değerleri merge edeceğiz.
+
+    // ************
+
+    // Form içindeki tüm inputların değerlerini almak için
+
+    const data = Object.fromEntries(formData.entries()); // Object.fromEntries() ile FormData.entries()'i Object'e çeviriyoruz.
+
+    // Ve acquisition değerlerini merge ediyoruz.
+
+    const mergedData = { ...data, acquisition };
+
+    // Veya şu şekilde de yapabiliriz.
+
+    // data.acquisition = acquisition;
+
+    console.log(mergedData);
+
+    // Bu şekilde form içindeki tüm inputların değerlerini alabiliriz.
+    // Multiple inputların aynı name'e sahip olması durumunda da merge edebiliriz.
+    //! Bu yöntemde önemli olan nokta, Inputların 'name' attribute'unun olmasıdır. Aksi takdirde FormData API'si ile değerlerini alamayız.
+  };
+
   return (
-    <form className='login-form h-fit'>
+    <form onSubmit={handleComplexFormSubmit} className='login-form h-fit'>
       <div className='flex flex-col items-center justify-center p-10'>
         <h1 className='h1-form'>Complex Signup Form</h1>
         <p className='p-form'>
@@ -74,21 +121,17 @@ const ComplexForm = () => {
             />
           </div>
         </div>
-        <div
-          className='select-box'
-          role='group'
-        >
+        <div className='select-box' role='group'>
           <label
             htmlFor='role'
             className='text-left text-white text-base font-mono'
           >
             What is your role?
           </label>
-          <select
-            name='role'
-            id='role'
-            className='select-form'
-          >
+          <select name='role' id='role' className='select-form'>
+            <option value='' disabled>
+              Select your role
+            </option>
             <option value='student'>Student</option>
             <option value='teacher'>Teacher</option>
             <option value='admin'>Admin</option>
@@ -148,24 +191,10 @@ const ComplexForm = () => {
           className='flex items-end justify-end gap-5 w-full my-5 text-white p-5 text-base font-mono'
           role='group'
         >
-          <button
-            type='submit'
-            className='submit-button'
-            onClick={(e) => {
-              e.preventDefault();
-              console.log('Form submitted.');
-            }}
-          >
+          <button type='submit' className='submit-button'>
             Submit
           </button>
-          <button
-            type='reset'
-            className='reset-button'
-            onClick={(e) => {
-              e.preventDefault();
-              console.log('Form reset.');
-            }}
-          >
+          <button type='reset' className='reset-button'>
             Reset
           </button>
         </div>
